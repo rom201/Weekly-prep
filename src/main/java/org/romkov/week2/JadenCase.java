077a3f33ef52f2913f201ec5b6834f9fbf28a6d6
rom201 <kovalchukr1104@gmail.com>
2023-06-18 15:02:04 -0400

    week2 nine tests was added

--- src/main/java/org/romkov/week2/JadenCase.java
+++ src/main/java/org/romkov/week2/JadenCase.java
@@ -6,40 +6,17 @@ import java.util.List;
 public class JadenCase {
     public static void main(String[] args) {
 
-//        List<Integer> numbers = Arrays.asList(4,5,3,9);
-//        int result = numbers.stream().reduce(0,(a,b) -> a+b);
-//        System.out.println(result);
-//
-//        //No initial value
-//        Optional<Integer> result2 = numbers.stream().reduce((a, b) -> a+b);
-//        System.out.println(result2);
-//
-//        //Max and Min
-//        Optional<Integer> min = numbers.stream().reduce(Integer::min);
-//        Optional<Integer> max = numbers.stream().reduce(Integer::max);
-//        Optional<Integer> sum = numbers.stream().reduce(Integer::sum);
-//
-//        System.out.println("Min:" + min.get());
-//        System.out.println("Max:" + max.get());
-//        System.out.println("Sum:" + sum.get());
-//
-//        //Task
-//        int dishCount = DishData.getAll().stream().map(d -> 1).reduce(0,(a,b) -> a+b);
-//        System.out.println(dishCount);
-//
-//        long dcount = DishData.getAll().stream().count();
-//        System.out.println(dcount);
 
 
-//        String input = "How can mirrors be real if our eyes aren't real";
+        String input = "How can mirrors be real if our eyes aren't real";
 //        String input = "";
-        String input = "f";
+//        String input = "f";
 //        String input = "  ";
 //        String input = "ST dgddgdg uru-ddhj r'try";
 //         String input = null;
 //        System.out.println(input);
-//        System.out.println(getJadenCasedStrings2(input));
-        System.out.println(input.substring(1));
+        System.out.println(getJadenCasedStrings2(input));
+//        System.out.println(input.substring(1));
 
     }
 
@@ -68,9 +45,7 @@ public class JadenCase {
             return "";
         }
 
-        List<String> strList = Arrays.asList(jString.split("\\s+"));
-        System.out.println(strList.size());
-
+        List<String> strList = Arrays.asList(jString.trim().split("\\s+"));
         return strList.stream().map(p -> p.substring(0, 1).toUpperCase().concat(p.substring(1).toLowerCase() + " "))
                 .reduce((a, b) -> a.concat(b)).get().trim();
 
---
+++ src/test/java/org/romkov/week2/JadenCaseTest.java
@@ -0,0 +1,84 @@
+package org.romkov.week2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JadenCaseTest {
+
+    @Test
+    void getJadenCasedStrings2_happy() {
+
+        String actual = "How can mirrors be real if our eyes aren't real";
+        String expected = "How Can Mirrors Be Real If Our Eyes Aren't Real";
+        Assertions.assertEquals(expected, JadenCase.getJadenCasedStrings2(actual));
+    }
+
+    @Test
+    void getJadenCasedStrings2_null() {
+
+        String actual = null;
+        String expected = "";
+        Assertions.assertEquals(expected, JadenCase.getJadenCasedStrings2(actual));
+    }
+
+    @Test
+    void getJadenCasedStrings2_empty() {
+
+        String actual = "";
+        String expected = "";
+        Assertions.assertEquals(expected, JadenCase.getJadenCasedStrings2(actual));
+    }
+    @Test
+    void getJadenCasedStrings2_oneBlank() {
+
+        String actual = " ";
+        String expected = "";
+        Assertions.assertEquals(expected, JadenCase.getJadenCasedStrings2(actual));
+    }
+
+    @Test
+    void getJadenCasedStrings2_manyBlank() {
+
+        String actual = "    ";
+        String expected = "";
+        Assertions.assertEquals(expected, JadenCase.getJadenCasedStrings2(actual));
+    }
+
+    @Test
+    void getJadenCasedStrings2_blanksBeforeAfter() {
+
+        String actual = " How can mirrors ";
+        String expected = "How Can Mirrors";
+        Assertions.assertEquals(expected, JadenCase.getJadenCasedStrings2(actual));
+    }
+
+    @Test
+    void getJadenCasedStrings2_blanksIn() {
+
+        String actual = "How can     mirrors";
+        String expected = "How Can Mirrors";
+        Assertions.assertEquals(expected, JadenCase.getJadenCasedStrings2(actual));
+    }
+
+    @Test
+    void getJadenCasedStrings2_singleChar() {
+
+        String actual = "t";
+        String expected = "T";
+        Assertions.assertEquals(expected, JadenCase.getJadenCasedStrings2(actual));
+    }
+
+    @Test
+    void getJadenCasedStrings2_capitals() {
+
+        String actual = " DC is   great";
+        String expected = "Dc Is Great";
+        Assertions.assertEquals(expected, JadenCase.getJadenCasedStrings2(actual));
+    }
+
+
+
+
+}
